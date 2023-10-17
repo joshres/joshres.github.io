94891096f6dce6eba48348b0e04657113864d50f
joshres <97273313+joshres@users.noreply.github.com>
2023-10-17 06:10:54 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -47,7 +47,7 @@ function displayModal() {
       <h2 class="modal-name">Josh Resnick</h2>
       <div class="modal-p-container">
         <p class="modal-about">Thank you for taking the time to get to know a bit more about me!</p>
-        <p>For the past 8 years, I have been working as a full-time cook. I have learned so much and have gained an immense amount of experience growing my skills and abilities. Some of these that I take pride in are my ability to communicate, attention to detail, adaptability, resourcefulness, and ability to work well under pressure. I thoroughly enjoy creating things and watching as they grow and evolve in to something beautiful and engaging.</p>
+        <p>For the past eight years, I have been working as a full-time cook. I have learned so much and have gained an immense amount of experience growing my skills and abilities. Some of these that I take pride in are my ability to communicate, attention to detail, adaptability, resourcefulness, and ability to work well under pressure. I thoroughly enjoy creating things and watching as they grow and evolve in to something beautiful and engaging.</p>
         <p>My personal hobbies include reading, writing, music, sports, hiking and more. I truly appreciate being able to be a part of something bigger than myself. I look forward to being able to work with a variety of talented and knowledgable individuals who will be able to help me grow as a worker and person as well.</p>
       </div> 
     </div>
